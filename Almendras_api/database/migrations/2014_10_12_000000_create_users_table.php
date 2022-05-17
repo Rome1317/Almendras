f5fa2cbbffad544d69874f1f3444cf810ec7e68a
Rome1317 <42395292+Rome1317@users.noreply.github.com>
2022-05-16 23:26:11 -0500

    database migration

--- Almendras_api/database/migrations/2014_10_12_000000_create_users_table.php
+++ Almendras_api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password');;
+            $table->string('street');
+            $table->string('CP');
+            $table->string('state');
+            $table->string('phone');
+            $table->unsignedBigInteger('role_id');
+            $table->string('ISO')->unique();
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('ISO')->references('ISO')->on('countries');
             $table->rememberToken();
             $table->timestamps();
         });
